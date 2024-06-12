70d4d79224627436cc5c45cac6ad45b3aee8d8a6
Leon Dhimba <dhimbaleon356@gmail.com>
2024-06-12 22:55:10 +0200

    add prediction serializers

---
+++ core/serializer.py
@@ -0,0 +1,34 @@
+from rest_framework import serializers
+
+
+class ConstructionPricePredictionSerializer(serializers.Serializer):
+    """
+    building height
+    builtup area
+    number of stories
+    number of columns
+    number of rooms
+    building function
+    number of units
+    total project cost
+    """
+    building_height = serializers.FloatField(required=True)
+    builtup_area = serializers.FloatField(required=True)
+    number_of_stories = serializers.IntegerField(required=True)
+    number_of_columns = serializers.IntegerField(required=True)
+    number_of_rooms = serializers.IntegerField(required=True)
+    building_function = serializers.CharField(required=True)
+    number_of_units = serializers.IntegerField(required=True)
+    total_project_cost = serializers.FloatField(required=True)
+
+
+class MaterialPricePredictionSerializer(serializers.Serializer):
+    """
+    inflationrate	imports	exports	moneysupplym1	BMI
+    """
+    inflation_rate = serializers.FloatField(required=True)
+    imports = serializers.FloatField(required=True)
+    exports = serializers.FloatField(required=True)
+    money_supply_m1 = serializers.FloatField(required=True)
+    bmi = serializers.FloatField(required=True)
+
