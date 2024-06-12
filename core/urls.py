d4703b545d338727d102df820270903e10d66d4a
Leon Dhimba <dhimbaleon356@gmail.com>
2024-06-12 22:54:51 +0200

    add material prediction url and view

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path, include
 
-from core.views import ConstructionPricePredictionAPIView
+from core.views import ConstructionPricePredictionAPIView, MaterialPricePredictionAPIView
 
 urlpatterns = [
-    path('predict', ConstructionPricePredictionAPIView.as_view(), name="predict-price"),
+    path('predict-construction-price', ConstructionPricePredictionAPIView.as_view(), name="predict-price"),
+    path('predict-material-price', MaterialPricePredictionAPIView.as_view(), name="predict-material-price"),
 ]
--- core/views.py
+++ core/views.py
@@ -4,6 +4,7 @@ from loguru import logger
 from rest_framework.permissions import AllowAny
 from rest_framework.views import APIView
 
+
 class ConstructionPricePredictionAPIView(APIView):
     permission_classes = [AllowAny]
     # serializer_class = LoanSerializer
@@ -14,3 +15,16 @@ class ConstructionPricePredictionAPIView(APIView):
         except Exception as e:
             logger.error(f"Server Error: {str(e.with_traceback())}")
             return JsonResponse({"error": str(e)}, status=500)
+
+
+class MaterialPricePredictionAPIView(APIView):
+    permission_classes = [AllowAny]
+    # serializer_class = LoanSerializer
+
+    def post(self, request):
+        try:
+            return JsonResponse({"message": "Hello World!"})
+        except Exception as e:
+            logger.error(f"Server Error: {str(e.with_traceback())}")
+            return JsonResponse({"error": str(e)}, status=500)
+
