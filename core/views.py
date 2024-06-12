70e5a1430846e4cc6c0daaa5b694c022ad9cbf9a
Leon Dhimba <dhimbaleon356@gmail.com>
2024-06-12 22:41:36 +0200

    add core app

---
+++ core/views.py
@@ -0,0 +1,16 @@
+from django.http import JsonResponse
+from django.shortcuts import render
+from loguru import logger
+from rest_framework.permissions import AllowAny
+from rest_framework.views import APIView
+
+class ConstructionPricePredictionAPIView(APIView):
+    permission_classes = [AllowAny]
+    # serializer_class = LoanSerializer
+
+    def post(self, request):
+        try:
+            return JsonResponse({"message": "Hello World!"})
+        except Exception as e:
+            logger.error(f"Server Error: {str(e.with_traceback())}")
+            return JsonResponse({"error": str(e)}, status=500)
