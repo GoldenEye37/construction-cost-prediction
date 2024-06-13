8886cb36d50a581f90d75d0be4fea54760c68bc3
Leon Dhimba <dhimbaleon356@gmail.com>
2024-06-13 14:16:01 +0200

    add preprocessor pkl file

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path, include
 
-from core.views import ConstructionCostPredictionAPIView, MaterialCostPredictionAPIView
+from core.views import ConstructionCostPredictionAPIView, MaterialCostPredictionAPIView, prediction_form
 
 urlpatterns = [
+    path('', prediction_form, name='prediction_form'),
     path('predict-construction-cost', ConstructionCostPredictionAPIView.as_view(), name="predict-Cost"),
     path('predict-material-cost', MaterialCostPredictionAPIView.as_view(), name="predict-material-Cost"),
 ]
